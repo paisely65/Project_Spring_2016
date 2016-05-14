0a6e2d5b877f68a820a87bbba6cd0ff5439021c8
paisely65 <spscuny@gmail.com>
2016-05-13 21:00:07 -0400

    Rename EPRS_Installation_Guide.py to Budget_code.py

---
+++ Budget_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module provides guidelines for installing an EPRS"""
+
+
+def projected_approval(estimated_costs, approved_budget):
+if approved_budget - estimated_cost > 27500:
+    return true
+else: 
+    false
+
+
+if project is approved:
+    then purchase and installation occurus
+else:
+    meetings continue with human resources
