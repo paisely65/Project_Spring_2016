f8fcbfdd6c957e015ec6979e4db7df60ca42b4db
paisely65 <spscuny@gmail.com>
2016-05-27 22:00:47 -0400

    Update Update_meds.py
    Syntax error adjusted.

--- Update_meds.py
+++ Update_meds.py
@@ -3,11 +3,12 @@
 """This module updates patient's information."""
 
 # Patient data is entered in this format after initial itake and updated when medication is assigned.
-{
-  "chart_number": {
+
+chart_number = {
     ('patient_name', 'date_of_birth', 'sex'): {
         'diagnosis': 'description',
         'admitted': 'admission_status'}
+    }
 
 admit_47283 = {
     ('Loan Peckham', 'May 2 1935', 'F'): {
@@ -37,75 +38,62 @@ admit_76126 = {
 admit_73277 = {
     ('Sandie Hendrickson', 'February 2 1945', 'F'): {
         'diagnosis': 'diabetes',
-        'admitted': 'Y'
+        'admitted': 'Y'}
     }
 admit_81823 = {
     ('Norman Konen', 'September 19 1945', 'M'): {
         'diagnosis': 'sprained ankle',
-        'admitted': 'N'
+        'admitted': 'N'}
     }
 admit_56187 = {
     ('Charlie Hartzler', 'November 11 1945', 'M'): {
         'diagnosis': 'hypertension',
-        'admitted': 'N'
+        'admitted': 'N'}
     }
 admit_44204 = {
     ('Bob Byerley', 'April 4 1949', 'M'): {
         'diagnosis': 'infection',
-        'admitted': 'Y'
+        'admitted': 'Y'}
     }
 admit_74227 = {
     ('Trang Fairfax', 'July 21 1954', 'M'): {
         'diagnosis':'infection',
-        'admitted': 'Y'
+        'admitted': 'Y'}
     }
 admit_78974 = {
     ('Elisha Abler', 'March 3 1955', 'M'): {
         'diagnosis': 'stroke',
-        'admitted': 'Y '
+        'admitted': 'Y '}
     }
 admit_64732 = {
     ('Ashlyn Toma', 'July 17 1958', 'F'): {
         'diagnosis': 'diabetes',
-        'admitted': 'N'
+        'admitted': 'N'}
     }
 admit_56547 = {
     ('Magan Bradstreet', 'August 6 1960', 'F'): {
         'diagnosis': 'hypertension',
-        'admitted': 'N'
+        'admitted': 'N'}
     }
 admit_18569 = {
     ('Guy Langlinais', 'September 9 1960', 'M'): {
         'diagnosis': 'diabetes',
-        'admitted': 'N'
+        'admitted': 'N'}
     }
 admit_68171 = {
     ('Cleopatra Cavender', 'March 3 1960', 'F'): {
         'diagnosis': 'lupus',
-        'admitted': 'Y'
+        'admitted': 'Y'}
     }
-}
+
 
 # Support staff can add new medication as dirceted by clinicians
 # This information represents a new screen on the user interface
 
 # adding meds template
-'chart_number['mediaction'] = ['name', 'strength','dosage']
-
-Examples
-
->>>
-{('Loan Peckham', 'May 2 1935', 'F'): {'admitted': 'Y', 'diagnosis':'hypertension'},
- 'mediaction': ['Celexa', '250mg', '1x daily as needed']}
-
->>>
-{('Guy Langlinais', 'September 9 1960', 'M'): {'admitted': 'N', 'diagnosis': 'diabetes'},
- 'mediaction': ['Insulin', '100-U', '>= 3x daily as needed']}
-
->>>
-{('Trang Fairfax', 'July 21 1954', 'M'): {'admitted': 'Y', 'diagnosis': 'infection'},
- 'mediaction': ['Amikacin', '15 mg', '15 mg/kg/day divided IV/IM q8-12hr']}
+#chart_number['mediaction'] = 'name_of_meds', 'strength','dosage'
+admit_68171["medication"] = 'Tylenol', '500mg', '2X daily'
 
 
 
-#Patient chart is immediatety updated and accessible to clinical staff
+
